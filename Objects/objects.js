513bfc15d738d2038b3ab1476a010ec3cb40496f
Abdul-Jabar <128983487+abduljabarcodes@users.noreply.github.com>
2024-02-22 09:18:08 +0300

    15th commit

--- Objects/objects.js
+++ Objects/objects.js
@@ -53,5 +53,45 @@ delete myCircle.draw;
 
 console.log(myCircle);
 
+                          
+
+//                                   Value  vs Reference Types
+
+let x = {value: 10};
+let y = x;
+
+x.value = 30;
+
+console.log(x); // output { value: 30 }
+console.log(y);  // output { value: 30 }
 
 
+//                      Enumerating Properties of an Object
+
+
+const circle5 = {
+  radius5: 1,
+  draw() {
+    console.log('draw');
+  }
+};
+
+
+for (let key in circle5) {
+  console.log(key + ':' + circle5[key]);
+}
+
+//                           Cloning an Object
+
+const circle6 = {
+  radius6: 1,
+  draw() {
+    console.log('draw');
+  }
+};
+
+const another = Object.assign({}, circle6);
+console.log(another);
+// OR
+const another2 = { ...circle6 };
+console.log(another);
