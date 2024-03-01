4a3d75e834f0bf1c5a26c7bdac21f61c5156b630
Abdul-Jabar <128983487+abduljabarcodes@users.noreply.github.com>
2024-03-01 20:33:40 +0300

    22nd commit

---
+++ Functions-/functions.js
@@ -0,0 +1,12 @@
+//                                   Function Declarations vs Expressions
+
+// Function Declaration
+function walk() {
+  console.log('walk');
+}
+
+walk();
+
+
+// function Expression
+let run = function() {};
