299dc9238b516838e720ad439acd3fa7b3376f0b
Abdul-Jabar <128983487+abduljabarcodes@users.noreply.github.com>
2024-03-04 21:53:29 +0300

    24th commit

--- Functions-/functions.js
+++ Functions-/functions.js
@@ -26,4 +26,48 @@ function sum(a, b) {
   return a + b;
 }
 
-console.log(sum(1, 2, 3, 4, 5));
+//console.log(sum(1, 2, 3, 4, 5));
+
+
+//                                      The Rest Operator
+
+function sum1(...args) {
+  return args.reduce((a, b) => a + b);
+}
+
+//console.log(sum1(1, 2, 3, 4, 5, 7, 10));
+
+
+//                                      Default Parameters
+
+function interest(principle, rate = 3.5, years = 3) {
+  return principle * rate / 100 * years;
+}
+
+//console.log(interest(10000));
+
+
+//                                     Getters And Setters
+
+
+// Getter => acces properties
+// Setters => change (mutate) them
+
+const person = {
+  firstName: 'Jabu',
+  lastName: 'Himo',
+  get fullName() {
+    return `${person.firstName} ${person.lastName}`;
+  },
+  set fullName(value) {
+    const parts = value.split(' ');
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  }
+};
+person.fullName = 'Jabu Himo';
+console.log(person);
+
+
+//                                         Try And Catch
+
