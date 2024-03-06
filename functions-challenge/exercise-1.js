46766d9d66ae2d4aafd665e56aeaa5c01176ef17
Abdul-Jabar <128983487+abduljabarcodes@users.noreply.github.com>
2024-03-06 09:05:43 +0300

    26th commit

---
+++ functions-challenge/exercise-1.js
@@ -0,0 +1,11 @@
+//                                      Sum of Arguments
+
+function sum(...items) {
+  if(items.length === 1 && Array.isArray(items[0]))
+    items = [...items[0]];
+
+
+  return items.reduce((a, b) => a + b);
+}
+
+console.log(sum(1, 2, 3, 4, 5, 6));
